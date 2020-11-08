cdb06d98ba6db77b4549fd50b5a83af17ce31e13
pparashar68 <pparashar68@gmail.com>
2020-11-07 20:09:33 -0800

    query4_employee_dept.sql

---
+++ EmployeeSQL/query4_employee_dept.sql
@@ -0,0 +1,5 @@
+SELECT 	employees.emp_no AS employee_number,employees.last_name,employees.first_name,
+		departments.dept_name AS department_name	
+FROM employees
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_emp.dept_no
